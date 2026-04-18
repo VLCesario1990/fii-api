e4ef5a42a7ac63590427a53b1c177eed1b8d77e3
VLCesario <vlcesario1990@gmail.com>
2026-04-18 14:54:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -24,6 +25,9 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless=new")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
